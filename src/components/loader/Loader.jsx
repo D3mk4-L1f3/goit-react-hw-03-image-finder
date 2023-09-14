f06f6780c3e26dcea31cda849cf1f1ecc574187a
D3mk4 <dmtvchenko@gmail.com>
2023-09-14 12:48:56 +0300

    update 1.3

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,13 +1,15 @@
-import { ThreeDots } from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
 
 export function Loader() {
   return (
-    <ThreeDots
+    <Circles
+      height="80"
+      width="80"
       color="#2e11d0"
-      width="200"
-      visible={true}
-      aria-label="loading..."
+      ariaLabel="loading..."
       wrapperStyle={{ margin: '0 auto' }}
+      wrapperClass=""
+      visible={true}
     />
   );
 }
